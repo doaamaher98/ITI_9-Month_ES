ec728f57ed2d7b2c566c88209853431232d3ea91
Doaa Maher <52929402+doaamaher98@users.noreply.github.com>
2022-11-25 00:48:57 +0200

    c++

---
+++ 09- Hackerrank_C++/A - For Loop.h
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    
+    int a,b;
+    
+    string num[] = {"one","two","three","four","five","six","seven","eight","nine"};
+    
+    cin>>a;
+    cin>>b;
+    
+    for(int n = a; n <= b ;n++)
+    {
+        if(n >= 1 and n <= 9 )
+        {
+            cout<<num[n-1]<<endl;
+        }
+        else
+        {
+            if(n % 2 == 0)
+            {
+                cout<<"even"<<endl;
+            }
+            else{
+                cout<<"odd"<<endl;
+            }
+        }
+        
+    }
+    
+    return 0;
+}
+    
